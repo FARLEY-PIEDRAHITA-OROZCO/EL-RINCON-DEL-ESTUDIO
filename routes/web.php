43b87dc13c2551e6b6eb2dfb876424080004330f
Farley Piedrahita Orozco <frlpiedrahita@gmail.com>
2023-10-30 13:25:11 -0500

    modificando pagina para mostrar a los muchachos

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,10 @@ use Illuminate\Support\Facades\Route;
 /* Rutas principales */
 Route::view('/', 'software');
 Route::view('/cursos', 'cursos');
+Route::view('/contacto', 'contacto');
+
+/* Envio de formulario */
+Route::post('/procesar_formulario', 'FormularioController@procesar');
 
 
 /* Rutas de la carpeta software */
