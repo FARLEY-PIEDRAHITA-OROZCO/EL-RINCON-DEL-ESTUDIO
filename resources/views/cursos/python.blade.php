5275025d397054c308f1524275e923172db0cb9b
Farley Piedrahita Orozco <frlpiedrahita@gmail.com>
2023-11-19 08:56:20 -0500

    añadiendo curso python

--- resources/views/cursos/python.blade.php
+++ resources/views/cursos/python.blade.php
@@ -4,7 +4,21 @@
 
 <section class="box-container-1">
             <div class="box-1">
-                <img src="/images/bases-de-datos/material1.png" alt="">
-                <a class="boton" href="/cursos/bases-de-datos/introduccion-a-bases-de-datos-mysql/introduccion"><p class="p-1">INTRODUCCIÓN A BASES DE DATOS MYSQL</p></a>
+                <img src="/images/curso-python/1.svg" alt="">
+                <a class="boton" href="/cursos/bases-de-datos/introduccion-a-bases-de-datos-mysql/introduccion"><p class="p-1">TÉRMINOS ASOCIADOS AL TIPO DE DATOS</p></a>
+            </div>
+
+            <div class="box-1">
+                <img src="/images/curso-python/3.png" alt="">
+                <a class="boton" href="/cursos/bases-de-datos/introduccion-a-bases-de-datos-mysql/introduccion"><p class="p-1">ENTRADA Y SALIDA DE DATOS CON PYTHON</p></a>
+            </div>
+
+            <div class="box-1">
+                <img src="/images/curso-python/4.png" alt="">
+                <a class="boton" href="/cursos/bases-de-datos/introduccion-a-bases-de-datos-mysql/introduccion"><p class="p-1">ESTRUCTURAS DE CONTROL CONDICIONALES</p></a>
+            </div>
+            <div class="box-1">
+                <img src="/images/curso-python/5.png" alt="">
+                <a class="boton" href="/cursos/bases-de-datos/introduccion-a-bases-de-datos-mysql/introduccion"><p class="p-1">CICLOS ITERATIVOS CON PYTHON</p></a>
             </div>
 @endsection
